93eb0d3cd6656f82a9b4ac7d7646438652b8c6d6
Akram Atassi <atassiakram@gmail.com>
2022-11-02 15:33:29 -0400

    doing something

---
+++ testing.java
@@ -0,0 +1,8 @@
+public class testing{
+	public static void main(String[] args){
+		System.out.println("Hello World");
+		System.out.println("Check123");
+
+		System.out.println(" Why do we exist");
+	}
+}
