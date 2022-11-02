d86e8097958387c969bf6bd3b4c39c74dda5d846
Akram Atassi <atassiakram@gmail.com>
2022-11-02 15:39:41 -0400

    Another Attempt

--- testing.java
+++ testing.java
@@ -1,8 +1,8 @@
 public class testing{
 	public static void main(String[] args){
-		System.out.println("Hello World");
-		System.out.println("Check123");
-		System.out.println("Go back");
-		System.out.println(" Why do we exist");
+		System.out.println("I don't wanna live");
+
+		System.out.println(" Hello world");
+
 	}
 }
