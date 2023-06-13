f4d4500cdf75da09f4b329c1da351492567ab468
Akram Atassi <atassiakram@gmail.com>
2023-06-13 05:55:12 +0400

    Removed 2 unnecessary classes, replaced it with MainClass

--- Class3.java
+++
@@ -1,8 +0,0 @@
-public class Class3 {
-    int num;
-
-    Class3(){}
-    Class3(int num){
-        this.num = num;
-    }
-}
---
+++ MainClass.java
@@ -0,0 +1,5 @@
+public class MainClass {
+    public static void printHelloWorld(){
+        System.out.println("Hello World!");
+    }
+}
