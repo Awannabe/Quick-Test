04739a8d5465deabbf861d263862d4b9ea83a9ca
Akram Atassi <atassiakram@gmail.com>
2023-06-13 05:23:24 +0400

    now prints Hello hunka chunka

--- Something.java
+++ Something.java
@@ -2,7 +2,8 @@ public class Something {
     public static void main(String[] args) {
         System.out.println("hello world!");
         System.out.println("Akram is hot");
-        System.out.print("Hi"); 
+        System.out.println("Hi");
+        System.out.println("Hello hunka chunka")
     }
     
 }
